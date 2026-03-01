066748b95df30ca3f7e29afe80133ff65bae2bfd
Naman Aarzoo <naman.spn17@gmail.com>
2026-03-01 17:24:46 +0530

    benchmarked and optimised prob1 solution...

--- prob1.cpp
+++ prob1.cpp
@@ -1,8 +1,22 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 #include <iostream>
-
 using namespace std;
 
+// v2 - Using arithmetic progression (AP) sum forumla, s = (n/2)(2a + (n-1)d)
+int main() {
+  int n = 999;
+  auto sum_of_multiples = [n](int x) {
+    int p = n / x;
+    return x * (p * (p + 1)) / 2;
+  };
+  cout << "Sum of multiples of 3 or 5 below 1000 is "
+       << sum_of_multiples(3) + sum_of_multiples(5) - sum_of_multiples(15)
+       << endl;
+  return 0;
+}
+
+// orignal v1, quick-bench results 4.1ms
+/*
 int main()
 {
     cout<<"test";
@@ -15,3 +29,4 @@ int main()
     cout<<"Sum is "<<sum<<endl;
     return 0;
 }
+*/
